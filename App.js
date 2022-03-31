69090b3163e191919905a05614627cef8a4aee1b
ElAgroudy <ahmed.elagroudy@vodafone.com>
2022-03-31 15:51:09 +0200

    using a working project

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './src/screens/Home';
+
+class App extends React.Component {
+  render() {
+    //return <Mainpage />;
+    return <Home />;
+  }
+}
+export default App;
